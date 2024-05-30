dc7bf9bddc5553ba7a20e23a3c414f540c5935ea
hugojohansson <carlhugojohansson@outlook.com>
2024-05-30 11:49:45 +0200

    Fix - analytics bugg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+
 
 
 
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>{children}
-      <GoogleAnalytics />
+      
       
       
       </body>
